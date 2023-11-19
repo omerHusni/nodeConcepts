fc104c13d36d6102ce4368af1a9d8e5b80c763bd
MassiveDynamic2 <omerhusni637@gmail.com>
2023-11-19 17:01:22 +0300

    Add UV_THREADPOOL_SIZE environment variable

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const crypto = require('crypto');
 
+process.env.UV_THREADPOOL_SIZE = 5;
+
 const start = Date.now();
 
 crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
@@ -14,7 +16,7 @@ crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
   console.log('3:', Date.now() - start);
 });
 
-crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', async () => {
+crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
   console.log('4:', Date.now() - start);
 });
 
