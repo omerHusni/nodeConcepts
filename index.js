6cc1d3cb2d8b14c67d775543ddaeb8c78cc058fe
MassiveDynamic2 <omerhusni637@gmail.com>
2023-11-22 21:07:28 +0300

    Refactor code to use Express.js for server setup

--- index.js
+++ index.js
@@ -1,25 +1,9 @@
-process.env.UV_THREADPOOL_SIZE = 5;
+const express = require('express');
 
-const crypto = require('crypto');
+const app = express();
 
-const start = Date.now();
-
-crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
-  console.log('1:', Date.now() - start);
+app.get('/', (req, res) => {
+  res.send('Hi there');
 });
 
-crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
-  console.log('2:', Date.now() - start);
-});
-
-crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
-  console.log('3:', Date.now() - start);
-});
-
-crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
-  console.log('4:', Date.now() - start);
-});
-
-crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
-  console.log('5:', Date.now() - start);
-});
+app.listen(3000);
