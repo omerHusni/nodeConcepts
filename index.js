6a0198c271cd8a17b85b123eb6d236575b138e2c
MassiveDynamic2 <omerhusni637@gmail.com>
2023-11-24 18:10:31 +0300

    Add cluster module for multi-threading

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
 const express = require('express');
+const cluster = require('cluster');
 
-const app = express();
+if (cluster.isMaster) {
+  // Cause index.js to be executed *again* but in child mode
+  cluster.fork();
+  cluster.fork();
+  cluster.fork();
+  cluster.fork();
+} else {
+  const app = express();
 
-function doWork(duration) {
-  const start = Date.now();
-  while (Date.now() - start < duration) {}
+  function doWork(duration) {
+    const start = Date.now();
+    while (Date.now() - start < duration) {}
+  }
+
+  app.get('/', (req, res) => {
+    doWork(5000);
+
+    res.send('Hi there');
+  });
+
+  app.get('/fast', (req, res) => {
+    res.send('This was fast!');
+  });
+
+  app.listen(3000);
 }
-
-app.get('/', (req, res) => {
-  doWork(5000);
-
-  res.send('Hi there');
-});
-
-app.listen(3000);
